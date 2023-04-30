65c888b640b73fbda9faff0e4234d4a9e7e429b4
jiriyu <jiriyu98@gmail.com>
2023-04-30 18:56:45 +0900

    feat: average return

--- paper/MAML/Agent.py
+++ paper/MAML/Agent.py
@@ -1,4 +1,5 @@
-import copy
+from time import sleep
+import gym
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,67 +7,129 @@ import numpy as np
 from collections import OrderedDict
 import matplotlib.pyplot as plt
 
+from torch.distributions import Categorical
+
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-class SineTask():
-    def __init__(self, amp, phase, min_x, max_x):
-        self.phase = phase
-        self.max_x = max_x
-        self.min_x = min_x
-        self.amp = amp
-
-    def sample_data(self, size=1):
-        x = np.random.uniform(self.max_x, self.min_x, size)
-        y = self.true_sine(x)
-        x = torch.tensor(x, dtype=torch.float).unsqueeze(1)
-        y = torch.tensor(y, dtype=torch.float).unsqueeze(1)
-        return x, y
-
-    def true_sine(self, x):
-        y = self.amp*np.sin(self.phase+x)
-        return y
+# ref: https://github.com/GauravIyer/MAML-Pytorch/blob/master/Experiment%201/Experiment_1_Sine_Regression.ipynb
 
 
-class SineDistribution():
-    def __init__(self, min_amp, max_amp, min_phase, max_phase, min_x, max_x):
-        self.min_amp = min_amp
-        self.max_phase = max_phase
-        self.min_phase = min_phase
-        self.max_amp = max_amp
-        self.min_x = min_x
-        self.max_x = max_x
+def generate_random_map(size=8, p=0.8):
+    """Generates a random valid map (one that has a path from start to goal)
+    :param size: size of each side of the grid
+    :param p: probability that a tile is frozen
+    """
+    valid = False
 
-    def sample_task(self):
-        amp = np.random.uniform(self.min_amp, self.max_amp)
-        phase = np.random.uniform(self.min_phase, self.max_phase)
-        return SineTask(amp, phase, self.min_x, self.max_x)
+    # DFS to check that it's a valid path.
+    def is_valid(res):
+        frontier, discovered = [], set()
+        frontier.append((0, 0))
+        while frontier:
+            r, c = frontier.pop()
+            if not (r, c) in discovered:
+                discovered.add((r, c))
+                directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+                for x, y in directions:
+                    r_new = r + x
+                    c_new = c + y
+                    if r_new < 0 or r_new >= size or c_new < 0 or c_new >= size:
+                        continue
+                    if res[r_new][c_new] == "G":
+                        return True
+                    if res[r_new][c_new] != "H":
+                        frontier.append((r_new, c_new))
+        return False
+
+    while not valid:
+        p = min(1, p)
+        # res = np.random.choice(["F", "H"], (size, size), p=[p, 1 - p])
+        res = ["SFFF", "FHFH", "FFFH", "HFFF"]
+        res = [list(x) for x in res]
+
+        g_x, g_y = np.random.choice(size, 2)
+        if g_x == 0 and g_y == 0:
+            continue
+
+        res[g_x][g_y] = "G"
+        valid = is_valid(res)
+    return ["".join(x) for x in res]
 
 
-class SineNet(nn.Module):
+class FrozenLakeSingleTask():
+    def __init__(self, net, desc) -> None:
+        self.env = gym.make('FrozenLake-v1', desc=desc)
+        self.net = net
+
+    def selectAction(self, action_prob):
+        m = Categorical(action_prob)
+        action = m.sample()
+        return action.item()
+
+    def sample_data(self, weight, size=100):
+        rollout = []
+        observation = self.env.reset()
+        for i in range(size):
+            action_prob = self.net.argforward(observation, weight)
+            action = self.selectAction(action_prob)
+            next_observation, extrinsic_reward, terminated, _ = self.env.step(
+                action)
+
+            rollout.append({"observation": observation,
+                            "action_prob": action_prob,
+                            "action": action,
+                            "extrinsic_reward": extrinsic_reward,
+                            "terminated": terminated,
+                            })
+            observation = next_observation
+
+            if terminated:
+                break
+
+        return rollout
+
+
+class FrozenLakeDistribution():
+    def __init__(self) -> None:
+        pass
+
+    def sample_task(self, net):
+        return FrozenLakeSingleTask(net, generate_random_map(size=4))
+
+
+class PolicyNet(nn.Module):
     def __init__(self):
-        super(SineNet, self).__init__()
+        super().__init__()
         self.net = nn.Sequential(OrderedDict([
-            ('l1', nn.Linear(1, 40)),
+            ('l1', nn.Linear(1, 64)),
             ('relu1', nn.ReLU()),
-            ('l2', nn.Linear(40, 40)),
+            ('l2', nn.Linear(64, 128)),
             ('relu2', nn.ReLU()),
-            ('l3', nn.Linear(40, 1))
+            ('l3', nn.Linear(128, 4)),
+            ('softMax1', nn.Softmax(dim=-1)),
         ]))
+
+    def forward(self, x):
+        x = torch.tensor(x, dtype=torch.float).unsqueeze(0).unsqueeze(0)
+        return self.net(x)
 
     # I implemented argforward() so that I could use a set of custom weights for evaluation.
     # This is important for the "inner loop" in MAML where you temporarily update the weights
     # of the network for a task to calculate the meta-loss and then reset them for the next meta-task.
+
     def argforward(self, x, weights):
+        x = torch.tensor(x, dtype=torch.float).unsqueeze(0).unsqueeze(0)
         x = F.linear(x, weights[0], weights[1])
         x = F.relu(x)
         x = F.linear(x, weights[2], weights[3])
         x = F.relu(x)
         x = F.linear(x, weights[4], weights[5])
+        x = F.softmax(x, dim=-1)
         return x
 
 
-class SineMAML():
+class CusMAML():
     def __init__(self, net, alpha, beta, tasks, k, num_metatasks):
         self.net = net
         self.weights = list(net.parameters())
@@ -75,113 +138,91 @@ class SineMAML():
         self.tasks = tasks
         self.k = k
         self.num_tasks_meta = num_metatasks
-        self.criterion = nn.MSELoss()
         self.meta_optimiser = torch.optim.Adam(self.weights, self.beta)
         self.meta_losses = []
-        self.plot_every = 100
         self.print_every = 100
         self.num_metatasks = num_metatasks
+        self.gamma = 0.99
+
+    def getLoss(self, rollout):
+        discounted_return = 0
+        returns = []
+        for tuple in rollout[::-1]:
+            discounted_return = discounted_return * \
+                self.gamma + tuple["extrinsic_reward"]
+            returns.append(discounted_return)
+
+        returns = returns[::-1]
+        returns = torch.tensor(returns)
+
+        policy_loss = []
+        for index in range(len(returns)):
+            log_prob, R = Categorical(rollout[index]["action_prob"]).log_prob(
+                torch.tensor(rollout[index]["action"])), returns[index]
+            policy_loss.append(-log_prob * R)
+
+        policy_loss = torch.cat(policy_loss).sum()
+
+        return discounted_return, policy_loss
 
     def inner_loop(self, task):
-        temp_weights = [w.clone() for w in self.weights]
-        x, y = task.sample_data(size=self.k)  # sampling D
-        x = x.to(device)
-        y = y.to(device)
-        output = self.net.argforward(x, temp_weights)
-        loss = self.criterion(output, y)/self.k
-        grads = torch.autograd.grad(loss, temp_weights)
-        # temporary update of weights
-        temp_weights = [w-self.alpha*g for w, g in zip(temp_weights, grads)]
-        x, y = task.sample_data(size=self.k)  # sampling D'
-        x = x.to(device)
-        y = y.to(device)
-        output = self.net.argforward(x, temp_weights)
-        metaloss = self.criterion(output, y)/self.k
-        return metaloss
+        metaloss = 0
+        discounted_return_sum = 0
+
+        for _ in range(self.k):
+            temp_weights = [w.clone() for w in self.weights]
+            rollout = task.sample_data(
+                weight=temp_weights)  # sampling D
+            discounted_return, loss = self.getLoss(rollout)
+            discounted_return_sum += discounted_return / self.k
+
+            loss = loss.to(device)
+            grads = torch.autograd.grad(loss, temp_weights)
+            # temporary update of weights
+            temp_weights = [w-self.alpha*g for w,
+                            g in zip(temp_weights, grads)]
+            rollout = task.sample_data(weight=temp_weights)  # sampling D'
+            _, metaloss_tmp = self.getLoss(rollout)
+            metaloss += metaloss_tmp
+
+        return discounted_return_sum, metaloss
 
     def outer_loop(self, num_epochs):
         total_loss = 0
+        average_return = 0
+
+        # line 2
         for epoch in range(1, num_epochs+1):
             metaloss_sum = 0
-            for i in range(self.num_metatasks):
-                task = self.tasks.sample_task()
-                metaloss = self.inner_loop(task)
+            discounted_return_sum = 0
+
+            # line 4 - line 9
+            for _ in range(self.num_metatasks):
+                task = self.tasks.sample_task(self.net)
+                discounted_return_tmp, metaloss = self.inner_loop(task)
                 metaloss_sum += metaloss
+                discounted_return_sum += discounted_return_tmp
+            metaloss_sum = metaloss_sum.to(device)
             metagrads = torch.autograd.grad(metaloss_sum, self.weights)
-            # important step
+
+            # line 10
             for w, g in zip(self.weights, metagrads):
                 w.grad = g
+
             ###############
             self.meta_optimiser.step()
-            total_loss += metaloss_sum.item()/self.num_metatasks
+            total_loss += metaloss_sum.item() / self.num_metatasks
+            average_return += discounted_return_sum / self.num_metatasks
             if epoch % self.print_every == 0:
-                print("{}/{}. loss: {}".format(epoch,
-                      num_epochs, total_loss / self.plot_every))
-            if epoch % self.plot_every == 0:
-                self.meta_losses.append(total_loss/self.plot_every)
+                print("{}/{}. loss: {}, average_return: {}".format(epoch,
+                      num_epochs, total_loss / self.print_every, average_return / self.print_every))
                 total_loss = 0
+                average_return = 0
 
 
-sine_tasks = SineDistribution(0.1, 5, 0, np.pi, -5, 5)
-net = SineNet()
+tasks = FrozenLakeDistribution()
+net = PolicyNet()
 net = net.to(device)
-maml = SineMAML(net, alpha=0.01, beta=0.001,
-                tasks=sine_tasks, k=5, num_metatasks=10)
+maml = CusMAML(net, alpha=0.01, beta=0.001,
+               tasks=tasks, k=5, num_metatasks=10)
 maml.outer_loop(num_epochs=500)
-plt.plot(maml.meta_losses)
-
-
-def test(og_net, x, y, lr, optim=torch.optim.SGD):
-    axis = np.linspace(-5, 5, 1000)
-    axis = torch.tensor(axis, dtype=torch.float)
-    axis = axis.to(device)
-    dummy_net = nn.Sequential(OrderedDict([
-        ('l1', nn.Linear(1, 40)),
-        ('relu1', nn.ReLU()),
-        ('l2', nn.Linear(40, 40)),
-        ('relu2', nn.ReLU()),
-        ('l3', nn.Linear(40, 1))
-    ]))
-    dummy_net = dummy_net.to(device)
-    dummy_net.load_state_dict(og_net.state_dict())
-    loss_fn = nn.MSELoss()
-    opt = optim(dummy_net.parameters(), lr=lr)
-    num_shots = 10
-    k = x.shape[0]
-    losses = []
-    outputs = {}
-    for i in range(num_shots):
-        out = dummy_net(x)
-        loss = loss_fn(out, y)
-        losses.append(loss.item())
-        dummy_net.zero_grad()
-        loss.backward()
-        opt.step()
-    outputs['minitrained'] = dummy_net(
-        axis.view(-1, 1)).detach().cpu().clone().numpy()
-    outputs['initial'] = og_net(torch.tensor(
-        axis).view(-1, 1)).detach().cpu().clone().numpy()
-    print(losses)
-    return outputs, axis
-
-
-def plot_test(og_net, x, y, task, optim=torch.optim.SGD, lr=0.01):
-    outputs, axis = test(og_net, x, y, lr, optim)
-    plt.figure(figsize=(10, 5))
-    plt.plot(axis.cpu().clone().numpy(), task.true_sine(
-        axis.cpu().clone().numpy()), '-', color=(0, 0, 1, 0.5), label='true sine')
-    plt.scatter(x.cpu().clone().numpy(), y.cpu().clone().numpy(), label='data')
-    plt.plot(axis.cpu().clone().numpy(), outputs['initial'], ':', color=(
-        0.7, 0, 0, 1), label='initial weights')
-    plt.plot(axis.cpu().clone().numpy(
-    ), outputs['minitrained'], '-', color=(0.5, 0, 0, 1), label='minitrained weights')
-    plt.legend(loc='lower right')
-    plt.show()
-
-
-K = 10
-task = sine_tasks.sample_task()
-x, y = task.sample_data(K)
-x = x.to(device)
-y = y.to(device)
-plot_test(og_net=maml.net.net, x=x, y=y, task=task)
